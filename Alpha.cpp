1d8ae46de7fa554c5e43461706fd2fd5df3aef20
Wajahat Shahid <wajahatdev@gmail.com>
2020-10-10 22:10:05 +0500

    Initiated Repository

---
+++ Alpha.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void IA()
+{
+	for(int i=0;i<10;i++)
+	cout<<"Peeno Mere Naal Weya Kr Ley"<<endl;
+	cout<<endl;
+}
+
+int main ()
+{
+	IA();
+	for (int i=0;i<5;i++)
+		cout<<"I love Pakistan."<<endl;
+	cout<<endl;
+	IA();
+	//Wajahat Shahid
+	bool flag;
+	flag= true;
+	if(flag)
+	cout<<"Bhai Nahana Parey gaa";
+	else
+	cout<<"Khaire hi hia Cheetey";
+}
